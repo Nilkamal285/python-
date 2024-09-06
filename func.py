03d30b31e38592273dba70b79765aab1c4c76cb9
Nilkamal285 <nilkamalpal43@gmail.com>
2024-09-06 21:38:41 +0530

    Add files via upload

--- func.py
+++ func.py
@@ -11,3 +11,8 @@ for x in range(-10, 11):
             min_x, min_y = x, y
 
 print(f"Minimum value: {min_value:.6f} at (x, y) = ({min_x:.2f}, {min_y:.2f})")
+
+
+
+#OPTIMIZATION: Python code optimization is a way to make your program perform any task more efficiently and quickly with fewer lines of code, less memory, or other resources involved, while producing the right results.
+#Types of optimization:	Linear optimization	Non-linear optimization	Constraint optimization	 Unconstraint optimization	Quadratic optimization
